eb7061393b1a063471fde25c1a5b28c502946e5c
balwan coding <balwansinghrajput1708@gmail.com>
2025-01-08 12:31:47 +0530

    change 2024 to 2025

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="box-border flex items-center justify-center p-4 text-white bg-indigo-600 ">
-      All &copy; Copyrights @2024
+      All &copy; Copyrights @2025
     </div>
   );
 }
