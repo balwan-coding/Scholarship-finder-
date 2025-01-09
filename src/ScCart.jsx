2e60959cc13cc3b8c32b3c089a9713c58ce5faee
balwan coding <balwansinghrajput1708@gmail.com>
2025-01-09 17:46:01 +0530

    add sum documeny deatil

--- src/ScCart.jsx
+++ src/ScCart.jsx
@@ -9,6 +9,28 @@ function ScCart({ scholar }) {
       <p className="text-gray-700">Gender: {scholar.Gender}</p>
       <p className="text-gray-700">Caste: {scholar.Cast}</p>
       <p className="text-gray-700">Education Level: {scholar.EducationLevel}</p>
+      <p className="font-bold text-center">Documents</p>
+      <div className="flex flex-wrap gap-1 m-1">
+        <span className="font-semibold text-gray-800">
+          Educational Certificate (Marksheet),
+        </span>
+        <span className="font-semibold text-gray-800">Income Certificate,</span>
+        <span className="font-semibold text-gray-800">Caste Certificate,</span>
+        <span className="font-semibold text-gray-800">
+          Domicile Certificate,
+        </span>
+        <span className="font-semibold text-gray-800">
+          Domicile Certificate,
+        </span>
+        <span className="font-semibold text-gray-800">Address Proof,</span>
+        <span className="font-semibold text-gray-800">Birth Certificate,</span>
+        <span className="font-semibold text-gray-800">
+          Identity Proof (Aadhar Card / Voter ID / Driving License),
+        </span>
+        <span className="font-semibold text-gray-800">
+          Passport (for studying abroad),
+        </span>
+      </div>
       <a
         className="text-blue-500 hover:text-red-600 animate-bounce"
         href={scholar.link || "#"}
