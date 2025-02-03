334e85a1caf835204917ee5b3e7783b9307fd76d
balwan coding <balwansinghrajput1708@gmail.com>
2025-02-03 14:59:02 +0530

    change input layout

--- src/SearchInput.jsx
+++ src/SearchInput.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 function SearchInput({ searchInput, handleSearchInputChange }) {
   return (
@@ -9,14 +10,17 @@ function SearchInput({ searchInput, handleSearchInputChange }) {
       >
         Search by Education Level
       </label>
-      <input
-        type="text"
-        id="searchInput"
-        value={searchInput}
-        onChange={handleSearchInputChange}
-        className="p-1 text-xl text-black bg-transparent border-2 border-r-4 border-indigo-800 rounded-2xl"
-        placeholder="Type education level (e.g., School)"
-      />
+      <div className="flex items-center gap-2 pr-1 bg-transparent border-2 border-indigo-800 rounded-xl">
+        <input
+          type="text"
+          id="searchInput"
+          value={searchInput}
+          onChange={handleSearchInputChange}
+          className="flex-1 p-2 text-lg text-black bg-transparent border-none focus:outline-none"
+          placeholder="Type education level (e.g., School)"
+        />
+        <FaSearch className="text-2xl text-gray-300 cursor-pointer" />
+      </div>
     </div>
   );
 }
