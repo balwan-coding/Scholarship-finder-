a3c926a4b5f29448466b72c96c3410a7e009fab9
balwan coding <balwansinghrajput1708@gmail.com>
2025-03-27 09:10:10 +0530

    navbar impro

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -24,7 +24,7 @@ function Navbar() {
         </button>
         <div className="fixed z-50 flex h-screen">
           <div
-            className={`fixed top-0 left-0 h-full w-52 bg-blue-600 text-white transition-all ${
+            className={`fixed top-0 left-0 h-full w-48 bg-blue-600 text-white transition-all ${
               isSidebarOpen ? "translate-x-0" : "-translate-x-full"
             }`}
           >
